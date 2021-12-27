3fbace3560d912505c3a3b132b09bebbdb2e9b72
AleIV <alejandrocorrales0201@gmail.com>
2021-12-27 13:42:41 -0600

    npc tool

--- src/main/java/me/aleiv/core/paper/Core.java
+++ src/main/java/me/aleiv/core/paper/Core.java
@@ -1,6 +1,9 @@
 package me.aleiv.core.paper;
 
+import com.comphenix.protocol.wrappers.WrappedGameProfile;
+
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import co.aikar.commands.PaperCommandManager;
@@ -8,7 +11,11 @@ import kr.entree.spigradle.annotations.SpigotPlugin;
 import lombok.Getter;
 import me.aleiv.core.paper.commands.GlobalCMD;
 import me.aleiv.core.paper.listeners.GlobalListener;
+import me.aleiv.core.paper.utilities.NegativeSpaces;
+import me.aleiv.core.paper.utilities.TCT.BukkitTCT;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import uk.lewdev.entitylib.FakeEntityPlugin;
+import uk.lewdev.entitylib.entity.FakePlayer;
 import us.jcedeno.libs.rapidinv.RapidInvManager;
 
 @SpigotPlugin
@@ -18,6 +25,7 @@ public class Core extends JavaPlugin {
     private @Getter Game game;
     private @Getter PaperCommandManager commandManager;
     private @Getter static MiniMessage miniMessage = MiniMessage.get();
+    private @Getter FakeEntityPlugin fakeEntityPlugin;
 
     @Override
     public void onEnable() {
@@ -26,6 +34,10 @@ public class Core extends JavaPlugin {
         RapidInvManager.register(this);
         BukkitTCT.registerPlugin(this);
         NegativeSpaces.registerCodes();
+
+        this.fakeEntityPlugin = new FakeEntityPlugin();
+
+        this.fakeEntityPlugin.onEnable();
 
         game = new Game(this);
         game.runTaskTimerAsynchronously(this, 0L, 20L);
@@ -46,6 +58,27 @@ public class Core extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        try {
+            this.fakeEntityPlugin.onDisable();
+
+        }catch (Exception e) {
+            
+        }
+    }
+
+    public void spawnClone(Player player) {
+        var loc = player.getLocation();
+        var property = WrappedGameProfile.fromPlayer(player).getProperties().entries().iterator().next().getValue();
+
+        var playerSkinValue = property.getValue();
+        var playerSkinSignature = property.getSignature();
+
+        var npc = FakePlayer.of(player.getName(), loc, playerSkinValue, playerSkinSignature);
+
+        var players = Bukkit.getOnlinePlayers();
+        for (var p : players) {
+            npc.show(p);
+        }
 
     }
 
